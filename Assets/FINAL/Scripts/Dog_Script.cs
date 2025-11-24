26ebbb9d19aac21fb4dc873e3110e1257278fe7b
Charles <charlescastro521@gmail.com>
2025-11-24 09:16:20 -0800

    movement
    misc

--- Assets/FINAL/Scripts/Dog_Script.cs
+++ Assets/FINAL/Scripts/Dog_Script.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class Dog_Script : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Misc.
+    public ParticleSystem PS;
+    public SpriteRenderer SR;
+    public AudioSource AS;
+
+    //Movement
+    public int decideMove;
+    public Vector3 startPos;
+    public Vector3 endPos;
+    public float delta = 1f;
+    public float Speed = 1f;
+
     void Start()
     {
+        startPos = transform.position;
+        decideMove = Random.Range(1, 3);
+        endPos.x = Random.Range(-3, 7);
+        endPos.y = startPos.y;
+    }
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, endPos, Speed * Time.deltaTime);
+
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Moveback()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, startPos, Speed * Time.deltaTime);
+    }
+
+    void OnMouseDown()
+    {
+
     }
 }
